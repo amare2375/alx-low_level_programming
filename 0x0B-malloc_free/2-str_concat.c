1f2dc516d4bcdb1e0700113fd9178af583d010b4
amare2375 <4amare@gmail.com>
2023-03-13 06:49:57 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +6,7 @@
  * str_concat - Entry point
  *@s1: string 1
  *@s2: string 2
- * Return: pointer should point to a newly allocated space in memory 
+ * Return: pointer should point to a newly allocated space in memory or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
